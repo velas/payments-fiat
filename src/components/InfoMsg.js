1653dd41bdf9f94fb340c7c0b95b95a905efb01f
Dmitrii Okhrimenko <dmytro.okhrimenko@gmail.com>
2022-08-04 16:33:19 +0300

    changed info about fee

--- src/components/InfoMsg.js
+++ src/components/InfoMsg.js
@@ -1,10 +1,9 @@
 export const title_info = `<h2 class="info-style-title">Buying Crypto with your Credit Card</h2>`;
-export const body = `<p class="info-style">The minimum transaction is $50 USD, and the maximum is $20,000 USD.<br>The provider charges a conversion and network fee. <br>Notice that the provider applies a minimum fee of $10 USD per transaction needed to ensure processing.<br>We do not collect any fees.</p>`;
+export const body = `<p class="info-style">The minimum transaction is $50 USD, and the maximum is $20,000 USD.<br>The provider charges a conversion and network fee. <br>Notice that the provider applies a minimum fee of $10 USD per transaction needed to ensure processing.</p>`;
 export const body_transak = `<p class="info-style">The minimum amount is $30 USD **, limit per transaction is $1,500 USD, daily limit per user is $14,000 USD, monthly limit per user is $28,000 USD, and yearly limit per user is $100,000 USD.
-<br>The provider charges a conversion and network fee. We do not charge any commissions.
+<br>The provider charges a conversion and network fee.
 <br>** For some cryptocurrencies our minimum buy amount may be greater due the minimum withdrawal limit of our partner exchanges.</p>`;
 export const body_utorg = `<p class="info-style">The minimum transaction is ~ $50 USD, and the maximum is ~ $10,000 USD.<br>The provider charges a conversion and network fee.
-<br>We do not collect any fees.
 </p>`;
 
 
